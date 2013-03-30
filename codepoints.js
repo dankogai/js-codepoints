bed052d69b50728d7a0fc727102942e7d5a7a5d2
Dan Kogai <dankogai+github@gmail.com>
2013-03-30 22:49:42 +0900

    All methods are now generic

--- codepoints.js
+++ codepoints.js
@@ -12,12 +12,18 @@
 (function(root) {
     'use strict';
     // utilities
-    var dummyfun = function(){};
-    var defineProperty = Object.defineProperty || dummyfun;
+    var defineProperty = Object.defineProperty;
     var install = function(o, k, f) {
-        if (o[k]) return;
-        o[k] = f;
-        defineProperty(o, k, {enumerable:false})
+        if (Object.defaultProperty) {
+            Object.defaultProperty(
+                o, k,
+                {value: f, writable: true, configurable: true}
+            );
+        } else {
+            if (o[k]) return;
+            o[k] = f;
+            if (defineProperty) defineProperty(o, k, {enumerable: false});
+        }
     };
     var fromCharCode = String.fromCharCode;
     // let the show begin
@@ -25,42 +31,42 @@
     install(RegExp, 'RE_UCHARS', re_uchars);
     // number of codepoints in the string -- needs ES5
     install(String.prototype, 'codePointsOf', function() {
-        return this.replace(re_uchars, '_').length;
+        return ('' + this).replace(re_uchars, '_').length;
     });
-    if (defineProperty !== dummyfun) {
+    if (defineProperty) {
         defineProperty(String.prototype, 'codepoints', {
-            get: function() { return this.codePointsOf() }
+            get: function() { return ('' + this).codePointsOf() }
         });
     }
     install(String.prototype, 'forEachUChar', function(f, ctx) {
-        var i = 0, s = this;
-        this.replace(re_uchars, function(u) {
+        var i = 0, s = '' + this;
+        s.replace(re_uchars, function(u) {
             f.call(ctx, u, i++, s);
         });
     });
     install(String.prototype, 'toArrayOfUChars', function() {
         var ret = [];
-        this.replace(re_uchars, function(cp){ ret.push(cp) });
+        ('' + this).replace(re_uchars, function(cp) { ret.push(cp) });
         return ret;
     });
     install(String.prototype, 'uCharAt', function(n) {
-        return this.toArrayOfUChars()[n]
+        return ('' + this).toArrayOfUChars()[n];
     });
     // ES6 proposals
     install(String, 'fromCodePoint', function(n) {
-        if (n < 0 && n > 0x10FFFF) 
+        if (n < 0 && n > 0x10FFFF)
             throw new RangeError('invalid codepoint');
         if (n < 0xFFFF) return fromCharCode(n);
         var offset = n - 0x10000,
-        hi = (offset >>>  10) + 0xD800,
+        hi = (offset >>> 10) + 0xD800,
         lo = (offset & 0x3FF) + 0xDC00;
         return fromCharCode(hi) + fromCharCode(lo);
     });
     install(String.prototype, 'codePointAt', function(n) {
-        var c = this.toArrayOfUChars()[n];
+        var c = ('' + this).toArrayOfUChars()[n];
         if (!c) return NaN;
         if (c.length === 1) return c.charCodeAt(0);
-        return 0x10000 
+        return 0x10000
             + (c.charCodeAt(0) - 0xD800) * 0x400
             + (c.charCodeAt(1) - 0xDC00);
     });
